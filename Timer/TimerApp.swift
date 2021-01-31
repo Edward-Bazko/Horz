8f90ab1497407ff03e1e440f0687c69ae67ca55a
Edward Bazko <edwardbazko@gmail.com>
2021-01-31 19:19:01 +0100

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Edward Bazko on 31/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
